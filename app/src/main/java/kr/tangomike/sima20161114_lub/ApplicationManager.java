7508a6adc595006cefaa94b0b41a44ef9398a981
shimaz <shimaz.bsh@gmail.com>
2016-11-07 16:47:55 +0900

    viewpager added

---
+++ app/src/main/java/kr/tangomike/sima20161114_lub/ApplicationManager.java
@@ -0,0 +1,16 @@
+package kr.tangomike.sima20161114_lub;
+
+import android.app.Application;
+
+/**
+ * Created by shimaz on 2016-11-07.
+ */
+
+public class ApplicationManager extends Application {
+
+    public void onCreate(){
+        super.onCreate();
+
+
+    }
+}
--- app/src/main/java/kr/tangomike/sima20161114_lub/MainActivity.java
+++ app/src/main/java/kr/tangomike/sima20161114_lub/MainActivity.java
@@ -1,13 +1,90 @@
 package kr.tangomike.sima20161114_lub;
 
 import android.app.Activity;
+//import android.app.Application;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends Activity {
+
+    private ApplicationManager apm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        super.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.layout_main);
+        apm = (ApplicationManager) getApplicationContext();
+
     }
+
+
+
+    public class MainPageAdapter extends PagerAdapter{
+
+        private LayoutInflater lf;
+
+        public MainPageAdapter(LayoutInflater inflater){
+            this.lf = inflater;
+        }
+
+
+        @Override
+        public int getCount(){
+            return 11;
+        }
+
+        public Object instantiateItem(ViewGroup container, int position){
+            View view;
+            view = lf.inflate(R.layout.layout_pager_main, null);
+            ImageView ivPager = (ImageView)view.findViewById(R.id.iv_main_pager);
+            String strImgName = "mainimg" + (position + 1);
+            ivPager.setImageResource(getResources().getIdentifier(strImgName, "drawable", getPackageName()));
+
+            container.addView(view);
+
+            return view;
+
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int arg1, Object arg2){
+
+            container.removeView((View)arg2);
+
+        }
+
+        @Override
+        public void destroyItem(View arg0, int arg1, Object arg2) {
+            ((ViewPager) arg0).removeView((View) arg2);
+
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+
+            return arg0 == ((View) arg1);
+
+
+        }
+
+        @Override
+        public Parcelable saveState() {
+            return null;
+        }
+
+    }
+
 }
